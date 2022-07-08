68dc5fadc432db33d1799ecfe60bfe13ff3b2ad5
mikiw <mikiw.pp@gmail.com>
2022-07-08 13:51:29 +0200

    Add render unit tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-it("renders ETH Balance", () => {
+test("Check 'Update blocks by date' button from EthereumCard.", ()=> {
+  const { getByTestId } = render(<App />);
+  const button = getByTestId("date-button");
+  expect(button).toBeInTheDocument();
+});
+
+test("Check 'Get balances and transactions' button from EthereumCard.", ()=> {
+  const { getByTestId } = render(<App />);
+  const button = getByTestId("transactions-button");
+  expect(button).toBeInTheDocument();
+});
+
+it("Renders '0 Ether' Balance from EthereumBalance.", () => {
   render(<App />);
-  expect(screen.getByText("ETH Balance")).toBeInTheDocument();
+  expect(screen.getByText("0 Ether")).toBeInTheDocument();
+});
+
+it("Renders 'Ethereum transactions' from EthereumTransactions.", () => {
+  render(<App />);
+  expect(screen.getByText("0 Ether")).toBeInTheDocument();
 });
